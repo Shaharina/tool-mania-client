6732ee5a6fc08c4e499973b073c0dbb28034d78a
Shaharina Mita <S.mita90232@gmail.com>
2022-08-03 18:56:02 +0600

    a

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import smallBanner from './';
+import bannerBg from '../../assets/image/banner-bg.jpg';
+import smallbanner from '../../assets/image/smallbanner.jpg';
 
 const banner = () => {
     return (
-        <div class='hero min-h-screen bg-base-200'>
-            <div class='hero-content flex-col lg:flex-row-reverse'>
-                <img src='https://placeimg.com/260/400/arch' class='max-w-sm rounded-lg shadow-2xl' />
+        <div className={`hero min-h-screen px-10`} style={{ background: `url(${bannerBg})`, backgroundSize: 'cover' }}>
+            <div className='hero-content flex-col lg:flex-row-reverse'>
+                <img src={smallbanner} alt='tools' className='lg:max-w-lg rounded-lg shadow-2xl' />
                 <div>
-                    <h1 class='text-5xl font-bold'>Box Office News!</h1>
-                    <p class='py-6'>Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class='btn btn-primary'>Get Started</button>
+                    <h1 className='text-5xl font-bold text-white'>MOST VERSATILE. MOST DURABLE.</h1>
+                    <p className='py-6 text-white'>Every company says it has the best people in the world, but when you look at our story over the last decade, the proof of that is glaringly apparent.</p>
+                    <button className='btn btn-primary text-white border-0'>Get Started</button>
                 </div>
             </div>
         </div>
