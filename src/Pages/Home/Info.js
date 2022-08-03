0569cb3c16a8b330dee2db120a29eafbcaa8573a
Shaharina Mita <S.mita90232@gmail.com>
2022-08-03 21:57:51 +0600

    product added

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Infocard from './Infocard';
 import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <Infocard img={clock}></Infocard>
-            <Infocard img={clock}></Infocard>
-            <Infocard img={clock}></Infocard>
+            <Infocard cardTitle='Opening Hours' bgClass=' ' img={clock}></Infocard>
+            <Infocard cardTitle='Our Locations' bgClass='bg-accent ' img={marker}></Infocard>
+            <Infocard cardTitle='Contact Us' bgClass=' ' img={phone}></Infocard>
         </div>
     );
 };
